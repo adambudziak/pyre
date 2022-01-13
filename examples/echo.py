9e7cc6e2427f01c03c4ed45781eb8fba48968982
Adam Budziak <adambudziak@protonmail.com>
2022-01-13 20:12:23 +0100

    Restructure catalogs

---
+++ examples/echo.py
@@ -0,0 +1,49 @@
+import dataclasses
+import json
+from dataclasses import dataclass
+
+from pyre.application import Scope, Pyre
+from pyre.injector import Injector
+from pyre.request import Request
+from pyre.router import Router
+from pyre.serializer import TextSerializer, JSONSerializer
+
+
+@dataclass
+class Body:
+    a: int
+    b: str
+    c: dict
+
+    @classmethod
+    def from_request(cls, request: "Request"):
+        d = json.loads(request.body)
+        return cls(**d)
+
+
+injector = Injector()
+injector.register(Body, Body.from_request)
+router = Router(serializer=JSONSerializer(), injector=injector)
+
+
+@router.get("/")
+async def a():
+    return {"result": "test"}
+
+
+@router.get("/text")
+async def b(r: Request):
+    r.response.serializer = TextSerializer()
+    return {"result": r}
+
+
+@router.post("/body")
+async def c(body: Body):
+    return {"body": dataclasses.asdict(body)}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    pyre = Pyre(router=router)
+    uvicorn.run(pyre)
